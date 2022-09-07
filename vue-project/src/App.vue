6ba4e28f66f03166f64fe02d8294c3f304330e15
Wendy Teo <wendyteo.wy@gmail.com>
2022-09-08 06:33:55 +1000

    Refactored and added styling

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,55 +1,28 @@
 <script>
 
+import UserProfile from './components/UserProfile.vue';
+
 export default {
-  data() {
-    return {
-      followers: 0,
-      user: {
-        id: 1,
-        username: '_WendyTeo',
-        firstName: 'Wendy',
-        lastName: 'Teo',
-        email: 'wteo@gmail.com',
-        admin: true
-      }
-    }
-  },
-  computed: {
-    fullName() {
-      return `${this.user.firstName} ${this.user.lastName}`;
-    }
-  },
-  watch: {
-    // watches data point when it changes and thus runs a functions
-    // similar to useEffect();
-    // pass two parameters that accepts the updated state value and the previous state value.
-    followers(newFollowerCount, oldFollowerCount) {
-      if (oldFollowerCount < newFollowerCount) {
-        console.log(`${this.user.username} has gained a follower!`);
-      }
-    }
-  },
-  methods: {
-    followUser() {
-      this.followers++;
-    }
-  },
-  mounted() {
-  // automatically renders functions for the first time
-  // lifecycle hooks 
-    this.followUser();
-  }
+  components: { UserProfile }
 }
 
 </script>
 
 <template>
-  <div>
-    @{{ user.username }} - {{ fullName}}
-    <strong>Followers:</strong> {{ followers }}
-    <button @click='followUser'>Follow</button>
+  <div id="app">
+    <UserProfile />
   </div>
 </template>
 
-<style scoped>
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  --webkit-font-smoothing: antialiased;
+  --moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  min-height: 100vh;
+  background-color: #F3F5FA;
+}
+
 </style>
---
+++ vue-project/src/components/UserProfile.vue
@@ -0,0 +1,83 @@
+<script>
+
+export default {
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: '_WendyTeo',
+        firstName: 'Wendy',
+        lastName: 'Teo',
+        email: 'wteo@gmail.com',
+        admin: true
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    }
+  },
+  watch: {
+    // watches data point when it changes and thus runs a functions
+    // similar to useEffect();
+    // pass two parameters that accepts the updated state value and the previous state value.
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower!`);
+      }
+    }
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    }
+  },
+  mounted() {
+  // automatically renders functions for the first time
+  // lifecycle hooks 
+    this.followUser();
+  }
+}
+
+</script>
+
+<template>
+    <div class="user-profile">
+        <div class="user-profile_user-panel">
+            <h1 class="user-profile_username">@{{ user.username }}</h1>
+            <div class="user-profile_follower-count">
+                <strong>Followers: </strong> {{ followers }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+
+.user-profile {
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    width: 100%;
+    padding: 50px 5%;
+}
+
+.user-profile_user-panel {
+    display: flex;
+    flex-direction: column;
+    margin-right: 50px;
+    padding: 20px;
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid #DFE3E8;
+}
+
+h1 {
+    margin: 0;
+}
+
+</style>
+
+
+
